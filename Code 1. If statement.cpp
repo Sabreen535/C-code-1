d5a55752f5b800e409c8b4ca07e6c1399d5f0d01
Sabreen535 <80199961+Sabreen535@users.noreply.github.com>
2021-03-07 17:03:24 +0500

    For beginner it very important to understand about control structure

---
+++ Code 1. If statement.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+   int mark;     //int is use for integer numbers
+   printf("\nenter your marks:");
+   scanf("%d",&mark);
+	if(mark>10)
+	{
+		printf("you pass the exam.");
+	}
+	else
+	{
+		printf("you fail the exam.");
+	}
+	return 0;
+	
+	
+	
+
+}
